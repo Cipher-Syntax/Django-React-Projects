ce7e4da9582e80f419d7fcb4995cbcab357c4192
Justine Toong <programmerjustine@gmail.com>
2025-09-01 15:21:14 +0800

    added not found page

--- Notes/frontend/src/pages/NotFound.jsx
+++ Notes/frontend/src/pages/NotFound.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const NotFound = () => {
+
     return (
-        <div>NotFound</div>
+        <div className='flex items-center justify-center h-[100vh]'>
+            <h1 className='font-bold text-[96px] animate-bounce'>NotFound</h1>
+        </div>
     )
 }
 
