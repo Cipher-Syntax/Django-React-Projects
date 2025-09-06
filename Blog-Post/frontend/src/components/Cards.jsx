321a233ffe1a1df1e318660f79dfb76758a4a642
Justine Toong <programmerjustine@gmail.com>
2025-09-06 14:44:29 +0800

    Added Card for latest posts

--- Blog-Post/frontend/src/components/Cards.jsx
+++ Blog-Post/frontend/src/components/Cards.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import image from "../assets/blog_post_image.jpg"
+import { IoIosArrowRoundForward } from "react-icons/io";
+
 
 const Cards = () => {
     return (
-        <div className='px-[60px]'>
+        <div className='mx-20 w-auto mt-20'>
             <h5>BROWSE AND READ THE LATEST STUFF</h5>
             <h2>Latest Stories</h2>
 
-
-            <div className='grid grid-cols-3 gap-8'>
-                <div className='w-[350px]'>
+            <div className='grid grid-cols-3 gap-8 mb-30'>
+                <div className='w-[350px] mb-30'>
                     <img src={image} alt="image" className='w-full h-[250px] object-cover'/>
                     <div className='bg-[#dfdfdf] absolute mt-[-50px] w-[300px]'>
                         <div className='w-full p-[10px]'>
@@ -25,18 +26,144 @@ const Cards = () => {
                             <div className='mt-5'>
                                 <p className='text-[12px]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad doloribus cum, in perspiciatis provident amet dignissimos sint ipsam es...</p>
                             </div>
+
+                            <div className='flex items-center justify-start mt-3 cursor-pointer text-[12px] gap-3 text-red-400 hover:text-red-500'>
+                                <p>READ MORE</p>
+                                <IoIosArrowRoundForward size={20} />
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div className='w-[350px]'>
-                    <img src={image} alt="image" />
-                </div>
+                <div className='w-[350px] mb-30'>
+                    <img src={image} alt="image" className='w-full h-[250px] object-cover'/>
+                    <div className='bg-[#dfdfdf] absolute mt-[-50px] w-[300px]'>
+                        <div className='w-full p-[10px]'>
+                            <h2 className='text-[20px] font-semibold'>The sunset faded to twilight</h2>
+                            <div className='flex gap-2 text-[12px] font-bold mt-2'>
+                                <p>09-06-2025</p>
+                                <p>Feature</p>
+                                <p>Comments</p>
+                            </div>
 
-                <div className='w-[350px]'>
-                    <img src={image} alt="image" />
+                            <div className='h-[2px] bg-red-400 w-[100px]'></div>
+
+                            <div className='mt-5'>
+                                <p className='text-[12px]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad doloribus cum, in perspiciatis provident amet dignissimos sint ipsam es...</p>
+                            </div>
+
+                            <div className='flex items-center justify-start mt-3 cursor-pointer text-[12px] gap-3 text-red-400 hover:text-red-500'>
+                                <p>READ MORE</p>
+                                <IoIosArrowRoundForward size={20} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className='w-[350px] mb-30'>
+                    <img src={image} alt="image" className='w-full h-[250px] object-cover'/>
+                    <div className='bg-[#dfdfdf] absolute mt-[-50px] w-[300px]'>
+                        <div className='w-full p-[10px]'>
+                            <h2 className='text-[20px] font-semibold'>The sunset faded to twilight</h2>
+                            <div className='flex gap-2 text-[12px] font-bold mt-2'>
+                                <p>09-06-2025</p>
+                                <p>Feature</p>
+                                <p>Comments</p>
+                            </div>
+
+                            <div className='h-[2px] bg-red-400 w-[100px]'></div>
+
+                            <div className='mt-5'>
+                                <p className='text-[12px]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad doloribus cum, in perspiciatis provident amet dignissimos sint ipsam es...</p>
+                            </div>
+
+                            <div className='flex items-center justify-start mt-3 cursor-pointer text-[12px] gap-3 text-red-400 hover:text-red-500'>
+                                <p>READ MORE</p>
+                                <IoIosArrowRoundForward size={20} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+            {/* Other grid */}
+            <div className='grid grid-cols-3 gap-8 mb-30'>
+                <div className='w-[350px] mb-30'>
+                    <img src={image} alt="image" className='w-full h-[250px] object-cover'/>
+                    <div className='bg-[#dfdfdf] absolute mt-[-50px] w-[300px]'>
+                        <div className='w-full p-[10px]'>
+                            <h2 className='text-[20px] font-semibold'>The sunset faded to twilight</h2>
+                            <div className='flex gap-2 text-[12px] font-bold mt-2'>
+                                <p>09-06-2025</p>
+                                <p>Feature</p>
+                                <p>Comments</p>
+                            </div>
+
+                            <div className='h-[2px] bg-red-400 w-[100px]'></div>
+
+                            <div className='mt-5'>
+                                <p className='text-[12px]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad doloribus cum, in perspiciatis provident amet dignissimos sint ipsam es...</p>
+                            </div>
+
+                            <div className='flex items-center justify-start mt-3 cursor-pointer text-[12px] gap-3 text-red-400 hover:text-red-500'>
+                                <p>READ MORE</p>
+                                <IoIosArrowRoundForward size={20} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='w-[350px] mb-30'>
+                    <img src={image} alt="image" className='w-full h-[250px] object-cover'/>
+                    <div className='bg-[#dfdfdf] absolute mt-[-50px] w-[300px]'>
+                        <div className='w-full p-[10px]'>
+                            <h2 className='text-[20px] font-semibold'>The sunset faded to twilight</h2>
+                            <div className='flex gap-2 text-[12px] font-bold mt-2'>
+                                <p>09-06-2025</p>
+                                <p>Feature</p>
+                                <p>Comments</p>
+                            </div>
+
+                            <div className='h-[2px] bg-red-400 w-[100px]'></div>
+
+                            <div className='mt-5'>
+                                <p className='text-[12px]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad doloribus cum, in perspiciatis provident amet dignissimos sint ipsam es...</p>
+                            </div>
+
+                            <div className='flex items-center justify-start mt-3 cursor-pointer text-[12px] gap-3 text-red-400 hover:text-red-500'>
+                                <p>READ MORE</p>
+                                <IoIosArrowRoundForward size={20} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className='w-[350px] mb-30'>
+                    <img src={image} alt="image" className='w-full h-[250px] object-cover'/>
+                    <div className='bg-[#dfdfdf] absolute mt-[-50px] w-[300px]'>
+                        <div className='w-full p-[10px]'>
+                            <h2 className='text-[20px] font-semibold'>The sunset faded to twilight</h2>
+                            <div className='flex gap-2 text-[12px] font-bold mt-2'>
+                                <p>09-06-2025</p>
+                                <p>Feature</p>
+                                <p>Comments</p>
+                            </div>
+
+                            <div className='h-[2px] bg-red-400 w-[100px]'></div>
+
+                            <div className='mt-5'>
+                                <p className='text-[12px]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad doloribus cum, in perspiciatis provident amet dignissimos sint ipsam es...</p>
+                            </div>
+
+                            <div className='flex items-center justify-start mt-3 cursor-pointer text-[12px] gap-3 text-red-400 hover:text-red-500'>
+                                <p>READ MORE</p>
+                                <IoIosArrowRoundForward size={20} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
