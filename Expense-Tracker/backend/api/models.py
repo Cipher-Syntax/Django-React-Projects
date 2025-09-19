118e8fabfd49c74186295cfdd553a9579ce192c0
aldrich030205 <aldrichzosobrado7@gmail.com>
2025-09-19 15:16:01 -0700

    Alter datetime field to datefield

--- Expense-Tracker/backend/api/models.py
+++ Expense-Tracker/backend/api/models.py
@@ -7,8 +7,8 @@ class Expense(models.Model):
     title = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=255, blank=True, null=True)
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
+    start_date = models.DateField()
+    end_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
