ef53165ace4a4477b96c5731803f3719dea1438c
aldrich030205 <aldrichzosobrado7@gmail.com>
2025-09-19 15:16:23 -0700

    Fixed: Filterting

--- Expense-Tracker/backend/api/views.py
+++ Expense-Tracker/backend/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions, generics #type: ignore
 from .models import Expense
 from .serializers import UserSerializer, ExpenseSerializer
+from django.utils.dateparse import parse_date
 
 # Create your views here.
 class CreateUser(generics.CreateAPIView):
@@ -21,7 +22,16 @@ class ExpenseViewSet(viewsets.ModelViewSet):
         end_date = self.request.query_params.get('end_date')
         
         if start_date and end_date:
-            queryset = Expense.objects.filter(start_date__gte=start_date, end_date__lte=end_date)
+            start = parse_date(start_date)
+            end = parse_date(end_date)
+            queryset = Expense.objects.filter(start_date__gte=start, end_date__lte=end)
+        elif start_date:
+            start = parse_date(start_date)
+            queryset = Expense.objects.filter(start_date__gte=start)
+        elif end_date:
+            end = parse_date(end_date)
+            queryset = Expense.objects.filter(end_date__lte=end)
+
         return queryset
     
     def perform_create(self, serializer):
