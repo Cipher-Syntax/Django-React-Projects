7d495651c0993d147ab50c7abaecf9ef8d40c959
Justine Toong <programmerjustine@gmail.com>
2025-09-28 19:32:10 +0800

    Added: cors-allowed-origins

--- Mini-Ecommerce/backend/backend/settings.py
+++ Mini-Ecommerce/backend/backend/settings.py
@@ -149,3 +149,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', cast=Csv())
