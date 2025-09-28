5c8e5b4aa02c57669fc19c112362155383d9f610
Justine Toong <programmerjustine@gmail.com>
2025-09-28 12:52:16 +0800

    Added: Backend and Frontend endpoints

---
+++ mini-eccommerce/frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { ProtectedRoute } from './components'
+import { Register, Login, Home, NotFound } from './pages'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+const Logout = () => {
+    localStorage.removeItem('access');
+    localStorage.removeItem('refresh');
+
+    return <Navigate to="/login"></Navigate>
+}
+
+const RegisterAndLogout = () => {
+    localStorage.removeItem('access');
+    localStorage.removeItem('refresh');
+
+    return <Register></Register>
+}
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/register' element={ <RegisterAndLogout></RegisterAndLogout>}></Route>
+                <Route path='/login' element={<Login></Login>}></Route>
+                <Route path='/logout' element={<Logout></Logout>}></Route>
+                <Route path='/' element={
+                    <ProtectedRoute>
+                        <Home></Home>
+                    </ProtectedRoute>
+                }>
+                </Route>
+                <Route path='*' element={<NotFound></NotFound>}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default App
---
+++ mini-eccommerce/frontend/src/components/index.js
@@ -0,0 +1,5 @@
+import ProtectedRoute from '../components/ProtectedRoute'
+
+export {
+    ProtectedRoute,
+}
